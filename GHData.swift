7a93d62228ffc5cb3d136b0c220649e6027f5e33
Mateusz Slaski <mateusz.slaski@ericsson.com>
2021-02-23 22:50:43 +0100

    Switch from camel case to snake case
    Some details

--- GHData.swift
+++ GHData.swift
@@ -1,43 +1,22 @@
 import Foundation
 
 struct GHData: Decodable {
-  let totalCount: Int
+  let total_Count: Int
   let items: [Repo]
-  
-  private enum CodingKeys: String, CodingKey {
-    case totalCount = "total_count"
-    case items = "items"
-  }
 }
 
 struct Repo: Decodable {
   let name: String
-  let isPrivate: Bool
+  let private: Bool
   let owner: Owner
   let description: String
-  let createdAt: String
-  let lastUpdate: String
+  let created_at: String
+  let last_update: String
   let language: String? //At some cases 'language' can be a nill, to avoid runtime errors that property must be optional
-  
-  private enum CodingKeys: String, CodingKey {
-    case name
-    case isPrivate = "private"
-    case owner
-    case description
-    case createdAt = "created_at"
-    case lastUpdate = "updated_at"
-    case language
-  }
 }
 
 struct Owner: Decodable {
   let login: String
-  let avatar: URL
-  let allRepositories: URL
-  
-  private enum CodingKeys: String, CodingKey {
-    case login
-    case avatar = "avatar_url"
-    case allRepositories = "repos_url"
-  }
+  let avatar_url: URL
+  let repos_url: URL
 }
